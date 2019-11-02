e6dfd4e35a7da7a96bd59cde67d9f239c653665f
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2019-11-02 11:57:31 -0700

    Tasks: Get rid of undefined behavior. Add user-friendly format. Add anime.js. Add second API.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,14 +23,11 @@ $(document).ready(function() {
 
             // Debugging
             // console.log(text);
-            // console.log(text.RelatedTopics);
-
-            // goThroughResults(text.RelatedTopics);
-            // $('#results').text(text.RelatedTopics[0].Text);
-
+            console.log(text.RelatedTopics);
+            $('#image').attr("src", text.RelatedTopics[0].Icon.URL);
             for (let i = 0; i < text.RelatedTopics.length; i++){
               if (typeof text.RelatedTopics[i].Text != undefined){
-                $('#results').append("<p>" + text.RelatedTopics[i].Text + "</p>");
+                $('#results').append("<p class='box'>" + text.RelatedTopics[i].Text + "</p>");
               }         
             }
           }).catch((err) => console.warn(err), $('#results').html("<p>It looks like the query didn't work. Did you try: </p><ul><li>Putting something in the search bar? (Don't leave it empty!)</li><li>Clearing your cache? (Duckduckgo is finicky sometimes.)</li></ul><p>If those don't work, try a different query. :)</p>"));

f1ca914349b4d013402c497b8a83af5bad636108
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2019-11-04 19:24:13 -0800

    Revert "Merge branch 'master' of https://github.com/deanmccluskey/Project-1"
    This reverts commit 15ef5cc3e8e8d035a7c9ea6fab5a0af873a9cf62, reversing
    changes made to 6118a0ad606b1f3537da63f12ebcf99d86931a6c.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,127 +1,39 @@
 let query = "";
 let queryURL = "";
 let text;
-// const CSStransforms = anime({
-//   targets: '#box .el',
-//   translateX: 250,
-//   scale: 2,
-//   rotate: '1turn'
-// });
-$(document).ready(function () {
-  $('form').on("submit", function () {
-    event.preventDefault();
-    query = $('#name').val();
-
-    // Debugging
-    // console.log("Query: ", query);
-
-    queryURL = "https://api.duckduckgo.com/?q=" + query + "&format=json&pretty=1";
-
-    // Debugging
-    // console.log("Query submitted.");
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      // console.log(response);
-      text = JSON.parse(response);
-
-      // Debugging
-      // console.log(text);
-      console.log(text.RelatedTopics);
-
-      // Call for Stiff Image (Use this if GIPHY doesn't work)
-      // $('#image').attr("src", text.RelatedTopics[0].Icon.URL);
-
-      // Empty the results so that we can append new ones from the new search.
-      $('#results').empty();
-
-      // Let's return only 3 results.
-      for (let i = 0; i < 3; i++) {
-
-        // Post will be an object containing different search results.
-        let post = text.RelatedTopics[i];
+$(document).ready(function() {
+    $('#submit').on("click", function() {
+        event.preventDefault();
+        query = $('#name').val();
 
         // Debugging
-        // console.log(post);
+        // console.log("Query: ", query);
 
-        // k cycles through the post object to check for specific properties
-        for (let k in post){
+        queryURL = "https://api.duckduckgo.com/?q=" + query + "&format=json&pretty=1";
 
-          // we want to append the text to the newly made box
-          if (k == "Text"){
-            $('#results').append("<div id='box" + i + "' class='box'></div>");
-
-            // we want to get rid of the text in front of each paragraph so it looks cleaner
-            let phrase = post.FirstURL.split("m/");
-
-            // Debugging
-            console.log("Phrase: ", phrase);
-            console.log("post[k]: ", post[k]);
-
-            // this gets rid of the text in front of the paragraph
-            post[k] = post[k].slice(phrase[1].length);
-
-            // this makes sure that no more added text is shown in front of the paragraph
-            let final = post[k].split("g)");
-
-            // Debugging
-            console.log("Phrase Length: ", phrase[1].length);
-
-            // this is for the case that we didn't need to do a split in the first place
-            if (final[1] != undefined){
-              $('#box' + i).append("<p class='description'>" + final[1] + "</p>");
-            } 
-            // this is if the split was successful
-            else {
-              $('#box' + i).append("<p class='description'>" + final[0] + "</p>");
-            }
-           
-            } 
-            // we grab the picture too if there is one
-            else if (k == "Icon"){
-
-              // Debugging
-              console.log("Photo: ", post[k].URL);
-              if (post[k].URL.length > 0){
-                $('#box' + i).append("<img class='photo' src='" + post[k].URL + "'>");
-              }
-             else {
-              $('#box' + i).append("<img class='photo' src='assets/images/missingpic.png'>");
-             }
-            }   
-          }
-        }
-          
-    }).catch((err) => {
-      console.warn(err);
-      $('#results').html("<p>It looks like the query didn't work. Did you try: </p><ul><li>Putting something in the search bar? (Don't leave it empty!)</li><li>Clearing your cache? (Duckduckgo is finicky sometimes.)</li></ul><p>If those don't work, try a different query. :)</p>")
-    });
-
-
-        let gifURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=9r7NcqflJmMVXoySzh8zg8XfGjFliOaS";
-
-        $.ajax({
-            url: gifURL,
+        // Debugging
+        // console.log("Query submitted.");
+    
+          $.ajax({
+            url: queryURL,
             method: "GET"
-        }).then(function (resp) {
-            // console.log(resp);
-            result = resp.data;
-            // console.log(result);
-            let counter = 0;
+          }).then(function(response) {
+            console.log(response);
+            text = JSON.parse(response);
 
-            while (result[counter].rating != "g") {
-                counter++;
+            // Debugging
+            // console.log(text);
+            // console.log(text.RelatedTopics);
+
+            // goThroughResults(text.RelatedTopics);
+            // $('#results').text(text.RelatedTopics[0].Text);
+
+            for (let i = 0; i < text.RelatedTopics.length; i++){
+              if (typeof text.RelatedTopics[i].Text != undefined){
+                $('#results').append("<p>" + text.RelatedTopics[i].Text + "</p>");
+              }         
             }
-
-            $('#image').attr("src", result[counter].images.fixed_height.url);
-
-
-
-        }).catch((err) => { console.warn(err); $('#image').html("<p>GIF Not Shown</p>") });
-
-
+          }).catch((err) => console.warn(err), $('#results').html("<p>It looks like the query didn't work. Did you try: </p><ul><li>Putting something in the search bar? (Don't leave it empty!)</li><li>Clearing your cache? (Duckduckgo is finicky sometimes.)</li></ul><p>If those don't work, try a different query. :)</p>"));
     });
 });
 
